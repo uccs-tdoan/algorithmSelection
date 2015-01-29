81a109eb2bc7a6131df037b4cb12021e586e2628
uccs-tdoan <tdoan@uccs.edu>
2015-01-29 14:51:09 -0800

    redesign graph

--- graph/graph1.R
+++ graph/graph1.R
@@ -15,31 +15,32 @@ realworld$selection <- ifelse(realworld$value > 0.6,'applicable','non-applicable
 synthetic$selection <- ifelse(synthetic$value > 0.6,'applicable','non-applicable')
 
 # Display value on top of bars
-#p1 <- ggplot(realworld,aes(x=variable,y= value,fill=selection)) +geom_bar(stat='identity') +
-   # ylab("Performance with SAR metric") + theme(axis.title.y=element_text(face='bold'))+
-   # facet_grid(Dataset~.) + geom_text(aes(label=round(value,2)),size=5, vjust=-0.2) +
-   # theme(axis.text.x=element_text(angle=90, size=10, vjust=0.5)) + scale_fill_grey(start = 0, end = .2)+
-   # theme(plot.title = element_text(size=20, face="bold", vjust=2)) +
-   # labs(x = "") + scale_y_continuous(breaks=c(0.25, 0.5, 0.75,1)) +
-   # geom_hline(aes(yintercept=0.6), colour="#990000", linetype="dashed")
-
+#p1 <- ggplot(realworld,aes(x=variable,y= value,fill=selection)) +
+#  geom_bar(stat='identity') + ylab("Performance with SAR metric") + 
+#  theme(axis.title.y=element_text(face='bold'))+
+#	ggtitle("Learners' performance on UCI datasets") + theme(legend.position="bottom")+
+#	facet_grid(Dataset~.) + scale_fill_grey(start = 0, end = .6)+
+#	theme(axis.text.x=element_text(angle=90, size=12,face="bold", vjust=0.5)) + 
+#  theme(plot.title = element_text(size=20, face="bold", vjust=2)) +
+#  labs(x = "") + scale_y_continuous(breaks=c(0.25, 0.5, 0.75,1)) +
+#	geom_hline(aes(yintercept=0.6), colour="black", linetype="dashed") +
+#  theme(strip.text = element_text(size=8)) 
+    
 p1 <- ggplot(realworld,aes(x=variable,y= value,fill=selection)) +
   geom_bar(stat='identity') + ylab("Performance with SAR metric") + 
-  theme(axis.title.y=element_text(face='bold'))+
-	ggtitle("Learners' performance on UCI datasets") + theme(legend.position="bottom")+
-	facet_grid(Dataset~.) + scale_fill_grey(start = 0, end = .6)+
-	theme(axis.text.x=element_text(angle=90, size=12,face="bold", vjust=0.5)) + 
+  theme(axis.title.y=element_text(face='bold'))+ theme(legend.position="bottom")+
+  facet_grid(Dataset~.) + scale_fill_grey(start = 0, end = .6)+
+  theme(axis.text.x=element_text(angle=90, size=12,face="bold", vjust=0.5)) + 
   theme(plot.title = element_text(size=20, face="bold", vjust=2)) +
   labs(x = "") + scale_y_continuous(breaks=c(0.25, 0.5, 0.75,1)) +
-	geom_hline(aes(yintercept=0.6), colour="black", linetype="dashed") +
+  geom_hline(aes(yintercept=0.6), colour="black", linetype="dashed") +
   theme(strip.text = element_text(size=8)) 
-    
-  
+
+
 p2 <- ggplot(synthetic,aes(x=variable,y= value,fill=selection)) + 
   geom_bar(stat='identity') + ylab("Performance with SAR metric") +
   theme(axis.title.y=element_text(face='bold')) + theme(legend.position="bottom")+
-  ggtitle("Learners' performance on synthetic datasets") +  facet_grid(Dataset~.) + 
-  scale_fill_grey(start = 0, end = .6)+
+  facet_grid(Dataset~.) + scale_fill_grey(start = 0, end = .6)+
   theme(axis.text.x=element_text(angle=90, size=12,face="bold", vjust=0.5)) +
   theme(plot.title = element_text(size=20, face="bold", vjust=2)) +
   labs(x = "") + scale_y_continuous(breaks=c(0.25, 0.5, 0.75,1)) +
@@ -51,6 +52,6 @@ print(p1)
 dev.off()
 
 pdf("graphSynthetic.pdf", width=6, height=5)
-print(p1)
+print(p2)
 dev.off()
  
